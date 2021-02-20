4e878338ab2d14209846f3bf24cf6506fe0b396a
David Hochstetler <daviddevs23@gmail.com>
2021-02-20 10:51:24 -0500

    Updated Balls for drawing

--- engine.py
+++ engine.py
@@ -1,5 +1,8 @@
 class Ball:
-    def __init__(self, id, x, y):
+    def __init__(self, id, x, y, color, isStriped, number):
         self.id = id
         self.pos = (x, y)
+        self.color = color # RGB values
+        self.isStriped = isStriped # True/False
+        self.number = number # The ball number
         
